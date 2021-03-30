fc67cc0ba7514f20cf294d2e8f3b561fa9a99dc1
JForestal1 <jforestal1@gmail.com>
2021-03-30 18:27:35 -0400

    added Note base class

---
+++ ElevenNote.Data/Note.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Data
+{
+    public class Note
+    {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid OwnerId { get; set; }
+
+        [Display(Name = "Note Title")]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Note Content")]
+        [Required]
+        public string Content { get; set; }
+
+        [Display(Name = "Date and Time Created")]
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date and Time Last Modified")] 
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
